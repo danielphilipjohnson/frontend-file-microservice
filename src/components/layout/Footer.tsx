8a282345b7c82f3cfad6b625a7ab542dadfe5850
Daniel Philip Johnson <daniel-philip-johnson@outlook.com>
2025-05-10 20:16:50 +0100

    feat: make layout

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,26 @@
+import StatusIndicator from "../ui/StatusIndicator";
+
+type CyberpunkFooterProps = {
+  version: string;
+  system: string;
+  product: string;
+};
+
+const CyberpunkFooter = ({
+  version,
+  system,
+  product,
+}: CyberpunkFooterProps) => {
+  return (
+    <footer className="mt-16 pt-8 border-t border-gray-800 text-sm text-gray-500">
+      <div className="flex justify-between items-center">
+        <div>
+          {system} • {product} {version}
+        </div>
+        <StatusIndicator status="CONNECTION SECURE" color="cyan" />
+      </div>
+    </footer>
+  );
+};
+
+export default CyberpunkFooter;
